d67e866a4d8510b43cf43e90ea0d4b37b99601c2
Jared Rollins <jared@jaredrollins.com>
2017-05-22 13:46:44 -0500

    Added soft delete

--- src/app/models/widget.model.ts
+++ src/app/models/widget.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+let softDelete = require('mongoose-soft-delete');
 
 const schema = new Schema({
   current: {
@@ -25,6 +26,8 @@ const schema = new Schema({
 }, {
     timestamps: true,
 });
+
+schema.plugin(softDelete, {index: true});
 
 /* Add methods and statics here */
 
